e54cbc098bc428b53a045e6460af168006825a7d
Joakim af Sandeberg <joakim@afsandeberg.se>
2015-09-14 17:04:11 +0200

    Just some tad little fixes. AM I RIGHT?!

--- src/main/java/se/kth/chord/FountainCoder/Main.java
+++ src/main/java/se/kth/chord/FountainCoder/Main.java
@@ -1,9 +1,12 @@
 package se.kth.chord.FountainCoder;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -11,13 +14,14 @@ public class Main {
 
 
 	private static final int NR_OF_BLOCKS = 1000;
-	private static final int BYTES_TO_READ = 8192000;
+	private static final int BYTES_TO_READ = 128000;
 	public static void main(String[] args) {
 
-		Path path = Paths.get("C:\\Users\\joakim\\Downloads\\seed147\\147\\test147_2.bin");
-		byte[] data= null;
+		Path path = Paths.get("C:\\Users\\joakim\\Downloads\\sv_windows_10_education_x64_dvd_6848214.iso");
+		byte[] data= new byte[BYTES_TO_READ];
 		try {
-			data = Files.readAllBytes(path);
+			InputStream reader = Files.newInputStream(path);
+            reader.read(data);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -35,7 +39,7 @@ public class Main {
 		Encoder enc = new Encoder(msg.length/blockSize, blockSize);
 
 		EncodingThread eThread = new EncodingThread(enc, msg);
-		DecodingThread dThread = new DecodingThread(enc);
+		DecodingThread dThread = new DecodingThread(enc, msg);
 		
 		eThread.start();
 		dThread.start();
@@ -88,13 +92,20 @@ class EncodingThread extends Thread {
 class DecodingThread extends Thread {
     
 	Encoder enc;
-	
-    DecodingThread(Encoder e) {
+	byte [] input;
+    DecodingThread(Encoder e, byte [] input) {
         enc = e;
+        this.input = input;
     }
 
     public void run() {
     	byte [] result = enc.decode();
         System.out.println("------ DECODED MESSAGE ------\n" + new String(result));
+        if(Arrays.equals(input, result)){
+            System.out.println("The arrays are the same!");
+        }
+        else{
+            System.out.println("The arrays differ");
+        }
     }
 }
